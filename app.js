f38e63dc1c5355fc6af53a1f382ab221d350d8d7
alex <alex@begly.co.uk>
2014-11-08 19:51:13 -0500

    Add basic execution

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var fs      = require('fs')
+var spawn   = require('child_process').spawn
 var express = require('express')
 var app     = express()
 var router  = express.Router()
 var io      = require('socket.io').listen(app.listen(3000))
+
 
 app.use('/', router)
 
@@ -11,6 +14,40 @@ router.route('/').get(function(req, res) {
 
 io.on('connection', function(socket) {
     socket.on('program-in', function(data) {
-        socket.emit('program-out', 'seems to work!')
+        writeFile(socket, data, executeFile)
     })
 })
+
+function executeFile(socket, path) {
+    cmd = spawn('python', [path])
+
+    cmd.on('error', function(err) {
+        console.error(path, err)
+    })
+
+    cmd.stdout.on('data', function(data) {
+        console.log(data.toString('utf-8'))
+        socket.emit('program-out', data.toString('utf-8'))
+    })
+
+    cmd.stderr.on('data', function(err) {
+        console.error(path, err)
+    })
+
+    cmd.on('close', function(code) {
+        console.log(path + ' exited with code: ' + code)
+    })
+}
+
+function writeFile(socket, data, callback) {
+    id = ("00000000", Math.floor(Math.random() * 4294967296).toString(16)).substr(-8)
+    path = '/tmp/' + id + '.py'
+    fs.writeFile(path, data, function(err) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('saved: ' + id)
+            callback(socket, path)
+        }
+    })
+}
